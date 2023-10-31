0498a47a2262c53fbc45fb5d41d04615f046f747
Uday <91021882+dDay-37@users.noreply.github.com>
2023-10-31 13:23:07 +0530

    Time: 620 ms (44.52%) | Memory: 29.2 MB (94.84%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.py
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.py
@@ -1,13 +1,5 @@
 class Solution(object):
     def findArray(self, pref):
-        """
-        :type pref: List[int]
-        :rtype: List[int]
-        """
-        res = []
-        if len(pref)!=0:
-            res.append(pref[0])
-            for i in range(1,len(pref)):
-                res.append(pref[i-1] ^ pref[i])
-            
-        return res
+        for i in range(len(pref) - 1, 0, -1):
+            pref[i] ^= pref[i - 1]
+        return pref
