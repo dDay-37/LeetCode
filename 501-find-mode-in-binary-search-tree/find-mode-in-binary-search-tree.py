f821f49da468c5ddbb6d0db21f19d764579f6c66
Uday <91021882+dDay-37@users.noreply.github.com>
2023-11-01 21:52:00 +0530

    Time: 49 ms (14.52%) | Memory: 21 MB (67.20%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
@@ -5,23 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    
-    def abc(self,root):
-        lis = []
-        if not root:
-            return
-        else:
-            print("root.val",root.val)
-            self.lis.append(root.val)
-            self.abc(root.left)
-            self.abc(root.right)
     def findMode(self, root):
         """
         :type root: TreeNode
         :rtype: List[int]
         """
         self.lis = []
-        self.abc(root)
+        def abc(root):
+            if not root:
+                return
+            else:
+                print("root.val",root.val)
+                self.lis.append(root.val)
+                abc(root.left)
+                abc(root.right)
+        abc(root)
         f = collections.Counter(self.lis)
         m = max(f.values())
         return [x for x in f if f[x] == m]
