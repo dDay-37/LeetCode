38b4091b89ba2b6c77dfede66edaf913ed6a235a
Uday <91021882+dDay-37@users.noreply.github.com>
2023-11-08 13:22:21 +0530

    Time: 10 ms (93.73%) | Memory: 13.3 MB (20.66%) - LeetSync

--- 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.py
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.py
@@ -8,22 +8,9 @@ class Solution(object):
         :type t: int
         :rtype: bool
         """
-        a=0
-        a+=min(abs(fx-sx),abs(fy-sy))
-        # print("a",a)
-        if fy-sy>0:
-            sy+=a
-        else:
-            sy-=a
-        if fx-sx>0:
-            sx+=a
-        else:
-            sx-=a
-        # print(sx,sy)
-        a+=max(abs(fx-sx),abs(fy-sy))
-        # print("a",a)
-        if a==0 and t==1:
-            return False
-        if a<=t:
-            return True
-        return False
+        if sx == fx and sy == fy:
+            if t == 1:
+                return False
+            else:
+                return True
+        return abs(sx - fx) <= t and abs(sy - fy) <= t
