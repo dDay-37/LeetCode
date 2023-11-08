f5bd14fa7a74d0c6ca0f40e0d282df2b4cbb1b35
Uday <91021882+dDay-37@users.noreply.github.com>
2023-11-08 16:32:00 +0530

    Time: 14 ms (74.91%) | Memory: 13.2 MB (46.86%) - LeetSync

--- 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.py
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.py
@@ -9,25 +9,24 @@ class Solution(object):
         :type t: int
         :rtype: bool
         """
-        a = 0  # Initialize the time taken to travel
-        a += min(abs(fx - sx), abs(fy - sy))  # Calculate and add the time taken to cover the minimum distance
+        a = 0 
+        a += min(abs(fx - sx), abs(fy - sy))
 
-        if fy - sy > 0:  # If the y coordinate difference is positive
-            sy += a  # Update sy by adding the value of a
+        if fy - sy > 0:
+            sy += a
         else:
-            sy -= a  # Otherwise, update sy by subtracting the value of a
+            sy -= a 
 
-        if fx - sx > 0:  # If the x coordinate difference is positive
-            sx += a  # Update sx by adding the value of a
+        if fx - sx > 0:
+            sx += a  
         else:
-            sx -= a  # Otherwise, update sx by subtracting the value of a
+            sx -= a  
+        a += max(abs(fx - sx), abs(fy - sy))
 
-        a += max(abs(fx - sx), abs(fy - sy))  # Calculate and add the time taken to cover the remaining distance
-
-        if a == 0 and t == 1:  # If a is 0 and t is 1, return False
+        if a == 0 and t == 1:  
             return False
 
-        if a <= t:  # If a is less than or equal to t, return True
+        if a <= t: 
             return True
 
-        return False  # Otherwise, return False
+        return False
