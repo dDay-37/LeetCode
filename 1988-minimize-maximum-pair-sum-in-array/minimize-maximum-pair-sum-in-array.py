698f9caa8631cb5c623cfc7924de83668b5585c2
Uday <91021882+dDay-37@users.noreply.github.com>
2023-11-17 12:21:38 +0530

    Time: 939 ms (68.83%) | Memory: 25.6 MB (24.68%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.py
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.py
@@ -4,8 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        n=len(nums)//2
         nums.sort()
         s=0
-        for i in range(len(nums)//2):
+        for i in range(n):
             s=max(s,nums[i]+nums[-i-1])
         return s
