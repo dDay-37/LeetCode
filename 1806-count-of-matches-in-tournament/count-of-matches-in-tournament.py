c3ddc56d00e74dccb3d19d8be88a77fbe6e70e0e
Uday <91021882+dDay-37@users.noreply.github.com>
2023-12-05 11:59:27 +0530

    Time: 17 ms (30.51%) | Memory: 13.5 MB (15.74%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
@@ -6,10 +6,7 @@ class Solution(object):
         """
         m=0
         while n!=1:
-            if n%2==0:
-                m+=n/2
-                n=n/2
-            else:
-                m+=(n-1)/2
-                n=(n+1)/2
+            m+=n//2
+            if n%2==1: n= (n+1)/2
+            else: n=n/2
         return m
