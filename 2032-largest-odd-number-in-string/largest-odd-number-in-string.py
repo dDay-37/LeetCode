0308c47346bbaec5ba883906b6c0db57eeabbdf9
Uday <91021882+dDay-37@users.noreply.github.com>
2023-12-07 13:46:06 +0530

    Time: 53 ms (46.21%) | Memory: 19.9 MB (6.50%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -4,7 +4,8 @@ class Solution(object):
         :type num: str
         :rtype: str
         """
+        a=["1","3","5","7","9"]
         for i in range(len(num)-1,-1,-1):
-            if int(num[i])%2 == 1:
+            if num[i] in a:
                 return "".join(num[:i+1])
         return ""
