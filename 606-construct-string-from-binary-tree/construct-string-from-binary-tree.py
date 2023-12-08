39f336b5010355903ab9aa104be570ce48843e87
Uday <91021882+dDay-37@users.noreply.github.com>
2023-12-08 13:08:43 +0530

    Time: 29 ms (67.97%) | Memory: 16.2 MB (19.53%) - LeetSync

--- 606-construct-string-from-binary-tree/construct-string-from-binary-tree.py
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.py
@@ -7,16 +7,16 @@
 class Solution(object):
     def tree2str(self, root):
         ans=[]
-        def DLR( root):
+        def f( root):
             if not root:
                 return
             ans.append('(')     
             ans.append(str(root.val)) 
             if not root.left and root.right:
                 ans.append('()')
-            DLR(root.left)
-            DLR(root.right)
+            f(root.left)
+            f(root.right)
             ans.append(')') 
-        DLR(root)
+        f(root)
         return "".join(ans)[1:-1]
         
