168008ebbef1e8b20573ae03c43d1aadda337c9f
Uday <91021882+dDay-37@users.noreply.github.com>
2023-12-12 12:39:31 +0530

    Time: 36 ms (27.99%) | Memory: 13.5 MB (8.65%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
@@ -1,18 +1,4 @@
-import heapq as heap
-
 class Solution(object):
     def maxProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        biggest = 0
-        second_biggest = 0
-        for num in nums:
-            if num > biggest:
-                second_biggest = biggest
-                biggest = num
-            else:
-                second_biggest = max(second_biggest, num)
-        
-        return (biggest - 1) * (second_biggest - 1) 
+        nums.sort()                    # Sort the array in non-decreasing order
+        return (nums[-1] - 1) * (nums[-2] - 1)   # Calculate the maximum product by subtracting 1 from the last two elements
