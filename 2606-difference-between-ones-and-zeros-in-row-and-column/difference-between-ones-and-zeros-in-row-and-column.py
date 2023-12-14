a1e6b41629d3ef53a55c02c9e09c01c022d4c0ee
Uday <91021882+dDay-37@users.noreply.github.com>
2023-12-14 19:21:36 +0530

    Time: 1162 ms (74.29%) | Memory: 44.5 MB (94.29%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.py
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.py
@@ -1,16 +1,21 @@
 class Solution(object):
     def onesMinusZeros(self, grid):
-        m, n = len(grid), len(grid[0])
-
-        r1 = [0] * m
-        c1 = [0] * n
-        for i in range(m):
-            for j in range(n):
-                r1[i] += grid[i][j]
-                c1[j] += grid[i][j]
-
-        for i in range(m):
-            for j in range(n):
-                grid[i][j] = 2 * (r1[i] + c1[j]) - m - n
-
+        row_count = len(grid)
+        col_count = len(grid[0])
+        row = [0]*row_count
+        col = [0]*col_count
+        i = 0
+        j = 0
+        for i in range(row_count):
+            row[i]=grid[i].count(1)
+        i = 0
+        j = 0
+        for i in range(col_count):
+            for j in range(row_count):
+                col[i]+=grid[j][i]
+        i = 0
+        j = 0
+        for i in range(row_count):
+            for j in range(col_count):
+                grid[i][j] = (2 * row[i] + 2 * col[j] - (row_count + col_count))
         return grid
