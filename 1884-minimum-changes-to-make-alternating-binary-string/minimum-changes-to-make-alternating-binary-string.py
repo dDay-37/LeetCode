7007e0149210a6010767de51eb3459adc6e5a212
Uday <91021882+dDay-37@users.noreply.github.com>
2023-12-24 09:31:29 +0530

    Time: 23 ms (86.76%) | Memory: 13.6 MB (48.53%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -5,16 +5,17 @@ class Solution(object):
         :rtype: int
         """
         n=len(s)
-        e0,e1,o0,o1=0,0,0,0
+        x=0
+        y=0
         for i in range(n):
             if i%2==0:
                 if s[i]=='0':
-                    e0+=1
+                    x+=1
                 else:
-                    e1+=1
+                    y+=1
             else:
                 if s[i]=='0':
-                    o0+=1
+                    y+=1
                 else:
-                    o1+=1
-        return n-max(o1+e0,o0+e1)
+                    x+=1
+        return n-max(x,y)
