d59b561175755a8f9301c32f673dec770066b0ef
Uday <91021882+dDay-37@users.noreply.github.com>
2023-12-26 23:48:27 +0530

    Time: 30 ms (95.34%) | Memory: 17.3 MB (5.86%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,9 +1,4 @@
 class Solution:
     def fib(self, n: int) -> int:
-        o=1
-        t=1
-        if n<2:
-            return n
-        for i in range(2,n):
-            t,o=o+t,t
-        return t
+        golden_ratio = (1 + 5 ** 0.5) / 2
+        return int((golden_ratio ** n + 1) / 5 ** 0.5)
