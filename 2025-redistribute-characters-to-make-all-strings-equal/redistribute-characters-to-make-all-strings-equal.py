439396c5ae4e21e91d7c9fea3c3a9e9057e9c209
Uday <91021882+dDay-37@users.noreply.github.com>
2023-12-30 23:52:46 +0530

    Time: 80 ms (11.11%) | Memory: 17.5 MB (9.52%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.py
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.py
@@ -1,18 +1,10 @@
 class Solution:
     def makeEqual(self, words: List[str]) -> bool:
-        """
-        Note that when string s1 is a permutation of string word s2, 
-        these two strings can Redistribute Characters to Make these 2 Strings Equal
-        """
-        n=len(words)
-        if n==1: return True
-        freq=[0]*26
-        for s in words:
-            for c in s:
-                freq[ord(c)-ord('a')]+=1
         
-        for f in freq:
-            if f%n!=0: return False
+        joint = ''.join(words)
+        set1 = set(joint)
+        
+        for i in set1 :
+            if joint.count(i) % len(words) != 0 : return False 
         return True
-
         
