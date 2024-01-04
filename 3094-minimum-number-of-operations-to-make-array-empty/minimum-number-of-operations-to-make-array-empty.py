29df8d4f00e008a88f72fe90f63a86bfe6f7180a
Uday <91021882+dDay-37@users.noreply.github.com>
2024-01-04 12:44:02 +0530

    Time: 587 ms (82.73%) | Memory: 31.9 MB (9.70%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
@@ -1,8 +1,28 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        mp =Counter(nums)
-        res =0 
-        for v in mp.values():
-            if v==1:return -1 
-            res +=math.ceil(v/3) 
-        return res 
+        nums.sort()
+        cr=nums[0]
+        c=1
+        ans=0
+        nums.append(-1)
+        print(nums)
+        for i in nums[1:]:
+            if i == cr:
+                c+=1
+            else:
+                if c==1:
+                    return -1
+                print(cr,c)
+                cr=i
+                if c%3==0:
+                    ans+=(c//3)
+                # elif c%3==2:
+                #     ans+=(c//3) + 1
+                # elif c%3==1:
+                #     ans+=(c//3) + 1
+                else:
+                    ans+=(c//3)+1
+                c=1
+        
+        return ans
+        
