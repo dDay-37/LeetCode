b64627f91dcae575f788b382fd2fc76b390cb202
Uday <91021882+dDay-37@users.noreply.github.com>
2024-01-22 23:34:21 +0530

    Time: 175 ms (47.76%) | Memory: 17.8 MB (94.40%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.py
@@ -0,0 +1,12 @@
+from collections import Counter
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        nums.append("-1")
+        x,y=-1,1
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+                x=nums[i]
+            if y==nums[i]:
+                y+=1
+        return [x,y]
