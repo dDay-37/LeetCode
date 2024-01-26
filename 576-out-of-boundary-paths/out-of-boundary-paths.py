4d45a3c7693fae53c09e4dcc7ea6becbac5581c8
Uday <91021882+dDay-37@users.noreply.github.com>
2024-01-26 23:27:39 +0530

    Time: 72 ms (91.91%) | Memory: 22.6 MB (34.68%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/out-of-boundary-paths.py
@@ -1,29 +1,14 @@
 class Solution:
-    def findPaths(self, m: int, n: int, N: int, x: int, y: int) -> int:
-        M = 1000000000 + 7
-        dp = [[0] * n for _ in range(m)]
-        dp[x][y] = 1
-        count = 0
-
-        for moves in range(1, N + 1):
-            temp = [[0] * n for _ in range(m)]
-
-            for i in range(m):
-                for j in range(n):
-                    if i == m - 1:
-                        count = (count + dp[i][j]) % M
-                    if j == n - 1:
-                        count = (count + dp[i][j]) % M
-                    if i == 0:
-                        count = (count + dp[i][j]) % M
-                    if j == 0:
-                        count = (count + dp[i][j]) % M
-                    temp[i][j] = (
-                        ((dp[i - 1][j] if i > 0 else 0) + (dp[i + 1][j] if i < m - 1 else 0)) % M +
-                        ((dp[i][j - 1] if j > 0 else 0) + (dp[i][j + 1] if j < n - 1 else 0)) % M
-                    ) % M
-
-            dp = temp
-
-        return count
-
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        @lru_cache(None)
+        def recursive_run(i,j,moves):
+            if i>=m or j>=n or i<0 or j<0:
+                return 1
+            elif moves == 0:
+                return 0
+            out = recursive_run(i+1,j,moves-1)
+            out += recursive_run(i-1,j,moves-1)
+            out += recursive_run(i,j+1,moves-1)
+            out += recursive_run(i,j-1,moves-1)
+            return out
+        return recursive_run(startRow,startColumn,maxMove) %(10**9+7)
