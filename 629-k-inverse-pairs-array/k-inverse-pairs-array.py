c86d464c74b31ce57a8d76bda335435e34738723
Uday <91021882+dDay-37@users.noreply.github.com>
2024-01-27 21:45:19 +0530

    Time: 166 ms (95.71%) | Memory: 16.8 MB (85.00%) - LeetSync

--- 629-k-inverse-pairs-array/k-inverse-pairs-array.py
+++ 629-k-inverse-pairs-array/k-inverse-pairs-array.py
@@ -1,14 +1,28 @@
 class Solution:
     def kInversePairs(self, n: int, k: int) -> int:
-        MOD = 10**9 + 7
-        dp = [[0] * (k + 1) for _ in range(n + 1)]
+        M = 1000000007
+        if k == 0:
+            return 1
+        if n == 1:
+            return 1 if k == 0 else 0
 
-        for i in range(1, n + 1):
-            for j in range(k + 1):
-                if j == 0:
-                    dp[i][j] = 1
-                else:
-                    val = (dp[i - 1][j] + MOD - (dp[i - 1][j - i] if j - i >= 0 else 0)) % MOD
-                    dp[i][j] = (dp[i][j - 1] + val) % MOD
+        current_count = [0] * (k + 1)
+        current_count[0] = 1
+        previous_count = [0] * (k + 1)
 
-        return (dp[n][k] + MOD - (dp[n][k - 1] if k > 0 else 0)) % MOD
+        for i in range(n - 2, -1, -1):
+            total_count = 0
+            threshold = n - i
+
+            for j in range(0, k + 1):
+                total_count += current_count[j]
+
+                if j >= threshold:
+                    total_count -= current_count[j - threshold]
+
+                total_count %= M
+                previous_count[j] = total_count
+
+            current_count, previous_count = previous_count, current_count
+
+        return current_count[k]
