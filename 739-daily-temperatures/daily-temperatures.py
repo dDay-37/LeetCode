bcc0f8a860c1b6333fcbd018923ddb373f0eea32
Uday <91021882+dDay-37@users.noreply.github.com>
2024-01-31 21:22:11 +0530

    Time: 229 ms (99.99%) | Memory: 29.1 MB (99.88%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -4,7 +4,7 @@ def dailyTemperatures(temperatures: List[int]) -> List[int]:
     output = [0]*len(temperatures)
     stack = []
     for cindex,ctemp  in enumerate(temperatures):
-        while stack and stack[-1][0] < ctemp:#cause it is monotonically decreasing
+        while stack and stack[-1][0] < ctemp:
                 t,i = stack.pop()
                 output[i] = cindex-i
         stack.append((ctemp, cindex))
