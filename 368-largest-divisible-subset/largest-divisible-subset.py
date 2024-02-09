a655d5874c7289c36bd40ceddee0d15652b392fd
Uday <91021882+dDay-37@users.noreply.github.com>
2024-02-09 22:04:20 +0530

    Time: 170 ms (98.68%) | Memory: 17.5 MB (41.66%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -1,25 +1,40 @@
 class Solution:
-    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+    def largestDivisibleSubsetMemoization(self, nums: List[int]) -> List[int]:
         nums.sort()
-        n = len(nums)
-        dp = [1] * n
-        max_size, max_index = 1, 0
 
-        for i in range(1, n):
-            for j in range(i):
-                if nums[i] % nums[j] == 0:
-                    dp[i] = max(dp[i], dp[j] + 1)
-                    if dp[i] > max_size:
-                        max_size = dp[i]
-                        max_index = i
+        @cache
+        def dfs(i, prev_index):
+            if i == len(nums):
+                return 0, -1
 
-        result = []
-        num = nums[max_index]
-        for i in range(max_index, -1, -1):
-            if num % nums[i] == 0 and dp[i] == max_size:
-                result.append(nums[i])
-                num = nums[i]
-                max_size -= 1
+            length1, ele1 = dfs(i+1, prev_index)
+            length2, ele2 = -1, -1
 
-        return result
+            if nums[i] % (nums[prev_index] if prev_index != -1 else 1) == 0:
+                length2, ele2 = dfs(i+1, i)
+                length2 += 1
+            
+            if length2 > length1:
+                ladder_map[nums[i]] = ele2
+                return length2, nums[i]
+            else:
+                return length1, ele1
 
+        _, starting_ele = dfs(0, -1)
+        res = []
+        while starting_ele != -1:
+            res.append(starting_ele)
+            starting_ele = ladder_map[starting_ele]
+        
+        return res
+    
+    def largestDivisibleSubset(self, nums):
+        # DP solution
+        # The container that holds all intermediate solutions.
+        # key: the largest element in a valid subset.
+        subsets = {-1: set()}
+        
+        for num in sorted(nums):
+            subsets[num] = max([subsets[k] for k in subsets if num % k == 0], key=len) | {num}
+        
+        return list(max(subsets.values(), key=len))
