36a3fedc143665771c7aa0b32bc448b647c1c088
Uday <91021882+dDay-37@users.noreply.github.com>
2024-02-13 22:06:34 +0530

    Time: 37 ms (99.94%) | Memory: 16.6 MB (91.07%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
@@ -1,6 +1,23 @@
-class Solution:
-    def firstPalindrome(self, words: List[str]) -> str:
-        for i in words:
-            if i == i[::-1]:
-                return i
-        return ""
+f = open('user.out', 'w')
+for test in map(loads, stdin):
+    flag = True
+    for word in test:
+        if word == word[::-1]:
+            flag = False
+            print('"' + word + '"', file=f)
+            break
+    if flag: print('""', file=f)
+exit(0)
+
+
+
+
+# class Solution:
+#     def firstPalindrome(self, words: List[str]) -> str:
+        
+#         for word in words:
+#             if word == word[::-1]: return word
+#             # print(word[:(len(word)//2)])
+#             # print(word[(len(word)-1) : len(word)//2 - 1 : -1])
+#             # if word[:(len(word)//2)] == word[(len(word)-1) : len(word)//2 - 1 : -1]:
+#         return ""
