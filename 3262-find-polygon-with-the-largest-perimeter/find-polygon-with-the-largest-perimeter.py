b5dd7ee344d67665e2469838b790ccd448e528d3
Uday <91021882+dDay-37@users.noreply.github.com>
2024-02-15 23:08:02 +0530

    Time: 456 ms (98.91%) | Memory: 31.5 MB (91.17%) - LeetSync

--- 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.py
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.py
@@ -1,17 +1,18 @@
-class Solution(object):
-    def largestPerimeter(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        nums.sort()  # Sort the list in ascending order
-        Csum = Psum = 0  # Initialize current sum and perimeter sum to 0
-
-        for i in range(len(nums)):
-            # Check if the current element can be part of a valid triangle
-            if nums[i] < Csum:
-                Psum = Csum + nums[i]  # Update perimeter sum with the potential triangle's perimeter
-            Csum += nums[i]  # Update current sum with the current element
-
-        # Return the largest perimeter if a valid triangle is found, otherwise return -1
-        return -1 if Psum == 0 else Psum
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        
+        
+        while 1:
+            if not nums: return -1
+            
+            maxx = max(nums)
+            suma = sum(nums) - maxx
+            
+            if suma<=maxx: nums.remove(maxx)
+            else: break
+                
+                
+        return sum(nums)
+                
+        
+        
