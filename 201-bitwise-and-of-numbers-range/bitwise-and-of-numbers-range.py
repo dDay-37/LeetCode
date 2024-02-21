8f6c529beaee377decaa59cd31539145ca658e61
Uday <91021882+dDay-37@users.noreply.github.com>
2024-02-21 21:25:42 +0530

    Time: 56 ms (42.90%) | Memory: 16.5 MB (81.69%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
@@ -1,5 +1,11 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        while (right > left):  # Loop until right is no longer greater than left
-            right = right & (right - 1) # Clear the least significant bit of right
-        return right 
+        
+       
+        while left < right:
+            
+            right = right & (right-1)
+      
+        return left & right
+          
+        
