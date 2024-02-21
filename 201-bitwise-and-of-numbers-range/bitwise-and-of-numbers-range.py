598f034eaac571d499bb9780edfc43f8853a95d4
Uday <91021882+dDay-37@users.noreply.github.com>
2024-02-21 21:24:23 +0530

    Time: 50 ms (69.48%) | Memory: 16.6 MB (81.69%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
@@ -1,11 +1,14 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        if right == 0:#in this case, left,right = 0,0
+            return 0
+
+        #find the biggest power of 2 less than or equal to right
+        closest_power_of_2 = 2**floor(log(right, 2))
+
         
-       
-        while left < right:
-            
-            right = right & (right-1)
-      
-        return left & right
-          
-        
+        if left < closest_power_of_2:
+            return 0
+        if left == closest_power_of_2:
+            return closest_power_of_2
+        return self.rangeBitwiseAnd(left - closest_power_of_2, right - closest_power_of_2) + closest_power_of_2
