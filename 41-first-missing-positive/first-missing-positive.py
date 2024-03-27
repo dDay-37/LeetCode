7be475f5568ae4e0f5d52df1cffee7262a5a277c
Uday <91021882+dDay-37@users.noreply.github.com>
2024-03-27 10:41:20 +0530

    Time: 276 ms (77.15%) | Memory: 27.8 MB (31.23%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,22 +1,10 @@
 class Solution(object):
     def firstMissingPositive(self, nums):
-        for i in range(len(nums)):
-            if nums[i]<0:
-                nums[i]=0
-        print("get rid of all neg and 0")
-        print(nums)
-        for i in range(len(nums)):
-            if nums[i]!=0:
-                id = abs(nums[i])-1
-                if id<len(nums):
-                    if nums[id]==0:
-                        nums[id]=float('-inf')
-                    else:
-                        nums[id]=-abs(nums[id])
-        mn=min(nums)
-        print("mark pos val, in range len(nums) as neg")
-        print(nums)
-        for i in range(len(nums)):
-            if nums[i]>=0:
-                return i+1
-        return -mn+1
+        nums=sorted(set(nums))
+        counter=0
+        for i in nums:
+            if i >0 :
+                counter+=1
+                if i != counter :
+                    return counter
+        return counter+1
