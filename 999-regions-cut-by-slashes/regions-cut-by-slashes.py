558bd405aa581e2560583797cf95edcf9e081b75
Uday <91021882+dDay-37@users.noreply.github.com>
2024-08-11 14:35:04 +0530

    Time: 203 ms (19.64%) | Memory: 18.7 MB (16.00%) - LeetSync

--- 999-regions-cut-by-slashes/regions-cut-by-slashes.py
+++ 999-regions-cut-by-slashes/regions-cut-by-slashes.py
@@ -4,7 +4,6 @@ class Solution:
         gx3=[[0]*3*col for _ in range(3*row)]
         for i in range(row):
             for j in range(col):
-                # print(grid[i][j])
                 if grid[i][j]=='/':
                     gx3[i*3][j*3+2]=1
                     gx3[i*3+1][j*3+1]=1
@@ -13,35 +12,20 @@ class Solution:
                     gx3[i*3][j*3]=1
                     gx3[i*3+1][j*3+1]=1
                     gx3[i*3+2][j*3+2]=1
-        # for l in gx3:
-        #     print(l)
         def dfs(r,c,visit):
             if (r<0 or c<0 or r==row*3 or c==col*3 or gx3[r][c]!=0 or (r,c) in visit):
                 return
-            # print((r,c))
             visit.add((r,c))
             dfs(r-1,c,visit)
             dfs(r,c-1,visit)
             dfs(r+1,c,visit)
             dfs(r,c+1,visit)
-        
         visit=set()
         res=0
         for i in range(row*3):
             for j in range(col*3):
                 if (gx3[i][j]==0 and (i,j) not in visit):
                     dfs(i,j,visit)
-                    # print("done")
                     res+=1
-        # print(visit)
         return res
-        # return -1
-        # dct={'/':[[0,0,1],[0,1,0],[1,0,0]], '\\':[[1,0,0],[0,1,0],[0,0,1]], ' ':[[0,0,0],[0,0,0],[0,0,0]]}
-        # gx3=[]
-        # for row in grid:
-        #     for x in row:
-        #         gx3.append(dct[x])
-
-        # for row in gx3:
-        #     print(row)
-        # return -1 
+ 
