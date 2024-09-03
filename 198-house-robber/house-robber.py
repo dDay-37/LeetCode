fde44bc9e33d18013cf63cbef735ba9b2d0f724e
Uday <91021882+dDay-37@users.noreply.github.com>
2024-09-03 23:46:17 +0530

    Time: 43 ms (10.71%) | Memory: 16.4 MB (69.34%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,9 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = [0] * (len(nums) +1)
-        memo[0] = 0
-        memo[1] = nums[0]
-        for i in range(1,len(nums)):
-            val = nums[i]
-            memo[i+1] = max(memo[i],memo[i-1] + val)
-        return memo[len(nums)]
+        # def f(i):
+        #     if i==0:
+        #         return nums[i]
+        #     elif i<0:
+        #         return 0
+        #     pick = nums[i]+f(i-2)
+        #     not_pick = f(i-1)
+        #     return max(pick,not_pick)
+        # return f(len(nums)-1)
+        if len(nums)==0:
+            return -1
+        if len(nums)==1:
+            return nums[0]
+        dp=[nums[0],max(nums[0],nums[1])]
+        for i in range(2,len(nums)):
+            dp.append(max(nums[i]+dp[i-2],dp[i-1]))
+        return dp[-1]
