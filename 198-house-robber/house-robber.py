bccc23502cace21787953f3d7bbedc395857f07b
Uday <91021882+dDay-37@users.noreply.github.com>
2024-09-03 23:49:33 +0530

    Time: 32 ms (79.40%) | Memory: 16.6 MB (27.93%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -9,11 +9,8 @@ class Solution:
         #     not_pick = f(i-1)
         #     return max(pick,not_pick)
         # return f(len(nums)-1)
-        if len(nums)==0:
-            return -1
-        if len(nums)==1:
-            return nums[0]
-        dp=[0,nums[0]]
-        for i in range(2,len(nums)+1):
-            dp.append(max(nums[i-1]+dp[i-2],dp[i-1]))
-        return dp[-1]
+        d = [0] * (len(nums)+1)
+        d[1] = nums[0]
+        for i in range(2, len(nums)+1):
+            d[i] = max(d[i-1], d[i-2]+nums[i-1])
+        return d[-1]
