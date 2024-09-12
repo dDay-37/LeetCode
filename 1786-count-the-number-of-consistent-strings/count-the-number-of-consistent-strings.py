538e7a84289080a3c71b4da8e16aadbfe8dc7631
Uday <91021882+dDay-37@users.noreply.github.com>
2024-09-12 23:31:16 +0530

    Time: 198 ms (87.95%) | Memory: 18.5 MB (83.05%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
@@ -2,12 +2,11 @@ class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
         a=set(allowed)
         # print(a)
-        c=0
         t=0
         for i in words:
             t+=1
             for l in i:
                 if l not in a:
-                    c+=1
+                    t-=1
                     break
-        return t-c
+        return t
