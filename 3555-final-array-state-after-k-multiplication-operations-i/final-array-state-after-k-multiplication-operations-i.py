4b165e0e0837d89b6f7c8d8f26c0cf8a5866ac9e
Uday <91021882+dDay-37@users.noreply.github.com>
2024-12-16 14:33:42 +0530

    Time: 7 ms (13.13%) | Memory: 17.4 MB (18.11%) - LeetSync

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
@@ -0,0 +1,6 @@
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        for i in range(k):
+            nums[nums.index(min(nums))]*=multiplier
+        return nums
+    
