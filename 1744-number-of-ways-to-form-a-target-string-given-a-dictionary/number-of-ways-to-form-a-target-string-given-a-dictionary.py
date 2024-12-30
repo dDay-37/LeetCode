cc6f40b3ab7588c06ff73d808a734e185111de66
Uday <91021882+dDay-37@users.noreply.github.com>
2024-12-30 22:33:01 +0530

    Time: 1454 ms (47.42%) | Memory: 382.1 MB (12.45%) - LeetSync

---
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numWays(self, words: List[str], target: str) -> int:
+        MOD = 10 ** 9 + 7
+        m, n = len(words[0]), len(target)
+        charAtIndexCnt = [[0] * m for _ in range(128)]
+        for word in words:
+            for i, c in enumerate(word):
+                charAtIndexCnt[ord(c)][i] += 1  # Count the number of character `c` at index `i` of all words
+
+        @lru_cache(None)
+        def dp(k, i):
+            if i == n:  # Formed a valid target
+                return 1
+            if k == m:  # Reached to length of words[x] but don't found any result
+                return 0
+            c = target[i]
+            ans = dp(k + 1, i)  # Skip k_th index of words
+            if charAtIndexCnt[ord(c)][k] > 0: # Take k_th index of words if found character `c` at index k_th
+                ans += dp(k + 1, i + 1) * charAtIndexCnt[ord(c)][k]
+                ans %= MOD
+            return ans
+
+        return dp(0, 0)
